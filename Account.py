bfe13e67de5c01ad4b5ead284178c85ec2e0363d
kstarek <41630229+kstarek@users.noreply.github.com>
2021-01-20 23:26:28 -0500

    Update Account.py

--- Account.py
+++ Account.py
@@ -8,7 +8,4 @@ class Account:
     api_key = os.environ.get('binance_api')
     api_secret = os.environ.get('binance_secret')
 
-    api_key = 'xbu0sYoMn227tJyEbJP956WwBi8eoeq4eZHs9UnMTq5LiBoRH66hR4Vy8RyRZhH5'  # temp solution delete after
-    api_secret = '2H9efSSmBsWv6UssTzgznD42hQBlhMAg5tLhcOhtVuZawxBra94Hcmff49ITDP52'
-
     client = Client(api_key, api_secret)
